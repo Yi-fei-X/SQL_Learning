881acf6dbac1bf3ed829290847cd5e59c8af8e4e
Yi-fei-X <81534639+Yi-fei-X@users.noreply.github.com>
2021-09-22 02:47:14 -0400

    Add files via upload

---
+++ SQLAssignment_4.sql
@@ -0,0 +1,21 @@
+/*
+1.	What is View? What are the benefits of using views?
+View is a virtual table. It's contents are defined by the query. Using a view you can customize the data you want for different purposes and make some user procedures easier. 
+2.	Can data be modified through views?
+Yes. The modification will be done on the base table.
+3.	What is stored procedure and what are the benefits of using it?
+Stored procedure group some TSQL statements into a logic unit and stored in the SQL server. By using the stored procedure, we can prevent the SQL injection and improve the performance.
+4.	What is the difference between view and stored procedure?
+View is a virtual table. Stored procedure is a group of TSQL statements.
+5.	What is the difference between stored procedure and functions?
+Stored precedure is mainly deal with the DML process and functions are mainly deal with calculation and expression.
+6.	Can stored procedure return multiple result sets?
+Yes. But you need to use the out parameters.
+7.	Can stored procedure be executed as part of SELECT Statement? Why?
+8.	What is Trigger? What types of Triggers are there?
+Trigger is a special type of stored precedure that can be executed when specific event happened. There are 3 types of DML trigger. 1. For. 2. After. 3.Instead of.
+9.	What are the scenarios to use Triggers?
+If you want to show some information when there are some specific execuations or when you want to automatically update some information.
+10.	What is the difference between Trigger and Stored Procedure?
+Trigger is a special type of stored precedure that can be automatically executed when specific event happened.
+*/
